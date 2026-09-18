1b4531cc053d9822209784c370748dabb2d9688e
mytvyt969-eng <mytvyt969@gmail.com>
2026-09-18 15:14:47 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    // QR Code Generation Library
+    implementation("com.google.zxing:core:3.5.3")
+
 }
